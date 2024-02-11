34583d58cf06ee7cf6c484c6a77a6ed300ef92e4
Murilo <munarimurilo441@gmail.com>
2024-02-11 12:22:26 -0300

    The testing phase with Postman is to verify if the endpoints are according to the specifications.

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,13 +11,14 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firtname;
+    private String firstname;
 
     private String lastname;
 
@@ -36,6 +35,12 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
-    public User() {
+    public User(UserDTO data){
+        this.firstname = data.firstName();
+        this.lastname = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+
     }
 }
---
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,63 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    public void createTransection(TransactionDTO transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+        if (this.authorizeTransaction(sender, transaction.value())){
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction newtransaction = new Transaction();
+        newtransaction.setAmount(transaction.value());
+        newtransaction.setSender(sender);
+        newtransaction.setReceiver(receiver);
+        newtransaction.setTimestamp(LocalDate.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+        this.repository.save(newtransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value){
+       ResponseEntity<Map> authorizationResponse= restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+       if (authorizationResponse.getStatusCode() == HttpStatus.OK){
+           String message = (String) authorizationResponse.getBody().get("message");
+           return "Autorizado".equalsIgnoreCase(message);
+
+       }else return false;
+    }
+}
