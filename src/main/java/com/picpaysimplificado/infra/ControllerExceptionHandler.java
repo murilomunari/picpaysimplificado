e6952ff5d40c1fa632649f6f7d1e81e1abb1bc3a
Murilo <munarimurilo441@gmail.com>
2024-02-11 20:28:40 -0300

    Project completed

---
+++ src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.infra;
+
+import com.picpaysimplificado.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário ja cadastrado", "400");
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(EntityNotFoundException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
--- src/main/java/com/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -26,38 +26,52 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private NotficationService notficationService;
 
-    public void createTransection(TransactionDTO transaction) throws Exception {
+    public Transaction createTransection(TransactionDTO transaction) throws Exception {
         User sender = this.userService.findUserById(transaction.senderId());
         User receiver = this.userService.findUserById(transaction.receiverId());
 
+        // Validação do saldo do remetente
         userService.validateTransaction(sender, transaction.value());
 
+        // Verificação e autorização da transação
         boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
-        if (this.authorizeTransaction(sender, transaction.value())){
+        if (!isAuthorized) {
             throw new Exception("Transação não autorizada");
         }
 
-        Transaction newtransaction = new Transaction();
-        newtransaction.setAmount(transaction.value());
-        newtransaction.setSender(sender);
-        newtransaction.setReceiver(receiver);
-        newtransaction.setTimestamp(LocalDate.now());
+        // Criação da transação
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transaction.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDate.now());
 
+        // Atualização dos saldos do remetente e do destinatário
         sender.setBalance(sender.getBalance().subtract(transaction.value()));
         receiver.setBalance(receiver.getBalance().add(transaction.value()));
 
-        this.repository.save(newtransaction);
+        // Salvamento da transação e atualização dos usuários
+        this.repository.save(newTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
+
+        // Envio de notificações
+        this.notficationService.sendNotfication(sender, "Transação realizada com sucesso");
+        this.notficationService.sendNotfication(receiver, "Transação recebida com sucesso");
+
+        return newTransaction;
     }
 
-    public boolean authorizeTransaction(User sender, BigDecimal value){
-       ResponseEntity<Map> authorizationResponse= restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
-       if (authorizationResponse.getStatusCode() == HttpStatus.OK){
-           String message = (String) authorizationResponse.getBody().get("message");
-           return "Autorizado".equalsIgnoreCase(message);
-
-       }else return false;
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else {
+            return false;
+        }
     }
 }
